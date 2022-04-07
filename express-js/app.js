a8d012b0799de6a81c661aa57f79138d560bdb33
muhammad rafi cahyaputra <71932553+Synnefo15@users.noreply.github.com>
2022-04-07 09:05:31 +0700

    express

---
+++ express-js/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+	// res.send('Halo Rafi'); // % text
+	// res.json({ // % Json
+	// 	nama : 'rafi',
+	// 	email : 'rafi@gmail.com',
+	// 	hp : '09138213'
+	// })
+	res.sendFile('./index.html', { root: __dirname });
+});
+
+app.get('/about', (req, res) => {
+	// res.send('Ini page about');
+	res.sendFile('./about.html', { root: __dirname });
+});
+app.get('/contact', (req, res) => {
+	// res.send('Ini page contact');
+	res.sendFile('/public/contact.html', { root: __dirname });
+});
+app.get('/product/:id', (req, res) => {
+	res.send(`Produk ID: ${req.params.id} <br> Category ID : ${req.query.category}`);
+});
+app.use('/', (req, res) => {
+	res.status(404);
+	res.send(/*html*/ ` <h1>404</h1> `);
+});
+
+app.listen(port, () => {
+	console.log(`Test att port ${port}`);
+});
